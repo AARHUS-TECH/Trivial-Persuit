20253ef46ae02ccd852320e0cc7bdc7afc34cecd
kasper kammer <kasperkammer@gmail.com>
2023-02-14 10:57:24 +0100

    fix my commit

--- view.php
+++ view.php
@@ -2,88 +2,53 @@
 
 include "config.php";
 
-$sql = "SELECT * FROM questions";
+$sql = "SELECT category_questions.Name, questions.Question, questions.Answer,  questions.DateCreated, role.Role, questions.Id FROM category_questions, questions, role WHERE questions.Category = category_questions.Id AND role.Id = questions.CreatedBy;";
 
 $result = $conn->query($sql);
 
 ?>
-
 <!DOCTYPE html>
-
 <html>
-
 <head>
-
     <title>View Page</title>
-
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 </head>
-
 <body>
-
     <div class="container">
-
-        <h2>Trivialpursuit</h2>
-
-<table class="table">
-
-    <thead>
-
+    <h2>Trivialpursuit</h2>
+    <table class="table">
+        <thead>
         <tr>
-
-        <th>Category nummer ID</>
-
+        <th>Category</>
         <th>Question</th>
-
         <th>Answer</th>
-
         <th>DateCreated</th>
-
         <th>CreatedBy</th>
-
-    </tr>
-
+        </tr>
     </thead>
-
-    <tbody> 
-
-        <?php
-
-            if ($result->num_rows > 0) {
-
-                while ($row = $result->fetch_assoc()) {
-
-        ?>
-
-                    <tr>
-
-                    <td><?php echo $row['Category']; ?></td>
-
-                    <td><?php echo $row['Question']; ?></td>
-
-                    <td><?php echo $row['Answer']; ?></td>
-
-                    <td><?php echo $row['DateCreated']; ?></td>
-
-                    <td><?php echo $row['CreatedBy']; ?></td>
-
-                    <td><a class="btn btn-info" href="update.php?id=<?php echo $row['Id']; ?>">Edit</a>&nbsp;<a class="btn btn-danger" href="delete.php?id=<?php echo $row['Id']; ?>">Delete</a></td>
-
-                    </tr>                       
-
-        <?php       }
-
-            }
-
-        ?>                
-
-    </tbody>
-
+<tbody> 
+<?php
+ if ($result->num_rows > 0) {
+ while ($row = $result->fetch_assoc()) {
+ ?>
+ <tr>
+    <td><?php echo $row['Name']; ?></td>
+    <td><?php echo $row['Question']; ?></td>
+    <td><?php echo $row['Answer']; ?></td>
+    <td><?php echo $row['DateCreated']; ?></td>
+    <td><?php echo $row['role']; ?></td>
+    <td><a class="btn btn-info" href="update.php?Id=<?php echo $row['Id']; ?>">Edit</a>&nbsp;<a class="btn btn-danger" href="delete.php?Id=<?php echo $row['Id']; ?>">Delete</a></td>
+ </tr>                       
+ <?php
+    }
+ }
+?>                
+</tbody>
 </table>
-
-    </div> 
-
+</div> 
 </body>
-
 </html>
+
+
+
+
